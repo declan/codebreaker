a6653e45647090ab2a55b35b420f6ec35a6d18d2
Declan Frye <deckleberryfrye@gmail.com>
2013-08-01 15:31:22 -0500

    single page version works, you can make a secret message and decode it all on the same page

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,24 @@
 function codeCtrl($scope) {
-  $scope.$watch('secretMessage', function() {
-    $scope.copyOfMessage = $scope.secretMessage;
-  });
+  $scope.translations = {};
+  var letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  for (var i = 0; i < letters.length; i++) {
+    $scope.translations[letters[i]] = false;
+  }
+  $scope.updateLetters = function() {
+    if (typeof($scope.secretMessage) != 'undefined') {
+      $scope.letters = [];
+      console.log($scope.secretMessage);
+      var letters = $scope.secretMessage.split('');
+      for (var i = 0; i < letters.length; i++) {
+        $scope.letters.push({letter: letters[i], translated: $scope.translations[letters[i]]});
+      }
+    }
+  }
+
+  $scope.translate = function(letter) {
+    $scope.translations[letter] = true;
+    $scope.updateLetters();
+  }
+
+  $scope.$watch('secretMessage', $scope.updateLetters);
 }
